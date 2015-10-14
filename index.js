946f8bfe017e87eef220b7eb214975a03be86cdc
Matías Lescano <matiasj.lescano@gmail.com>
2015-10-14 15:18:49 -0300

    serve book

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
+var express = require('express')
+var port = process.env.PORT || 3000
+var app = express()
 
 app.get('/', function(request, response) {
-    response.sendfile(__dirname + '/site/index.html');
+    response.sendfile(__dirname + '/site/index.html')
 }).configure(function() {
-    app.use('/', express.static(__dirname + '/site/'));
-}).listen(port);
+    app.use('/', express.static(__dirname + '/site/'))
+    app.use('/book', express.static(__dirname + '/book/'))
+}).listen(port)
